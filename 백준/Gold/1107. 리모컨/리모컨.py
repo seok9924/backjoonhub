dca04602ebdcf10b0bab69a8283681995838e1f3
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-05-04 17:25:20 +0900

    [Gold V] Title: 리모컨, Time: 284 ms, Memory: 115280 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,17 @@
+n = int(input())
+ans = abs(100 - n)
+m = int(input())
+if m:
+    broken = set(input().split())
+else:
+    broken = set()
+
+
+for i in range(1000001):
+    for j in str(i):
+        if j in broken:
+            break
+    else:
+        ans = min(ans, len(str(i)) + abs(i - n))
+
+print(ans)
