6c52dcd167c6307e0ebef60ee07c01e7ffd8fe2e
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-05-14 23:54:07 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,14 @@
+n = int(input())
+white = [[0] * 100 for _ in range(100)]  
+for _ in range(n):
+    y1, x1 = map(int, input().split())
+
+    for i in range(x1, x1 + 10):
+        for j in range(y1, y1 + 10):
+            white[i][j] = 1
+
+result = 0
+for k in range(100):
+    result += white[k].count(1)
+
+print(result)
